c9df0a0ec30084fe70f9b54212a8c15bfcd4f65c
Steve <sthlm_32@hotmail.com>
2025-03-21 15:31:52 +0100

    Dataframe added

--- stalarm.py
+++ stalarm.py
@@ -1,6 +1,7 @@
 import yfinance as yahooFinance
 import os
 import configparser
+import pandas as pd
 
 os.system('cls')
 
@@ -20,12 +21,31 @@ def read_config_ini():
 
 symbols, alarm_limit = read_config_ini()
 
+stock_data = []
+
 for symbol in symbols:
     stock_info = yahooFinance.Ticker(symbol)
     print(symbol)
+    company_name = stock_info.info.get("longName", "N/A")
+    print(company_name)
     latest_data = stock_info.history(period="1d")
     print(latest_data["Open"].iloc[-1]) # -1 means "the last row" in the DataFrame
+    opening_value = latest_data["Open"].iloc[-1] # -1 means "the last row" in the DataFrame
+    last_value = stock_info.fast_info["last_price"]
     print(stock_info.fast_info["last_price"])  # Latest stock price
+
+    stock_data.append({
+        "symbol": symbol,
+        "company_name": company_name,
+        "opening_value": opening_value,
+        "last_value": last_value
+    })
+
+# Create a DataFrame with the collected data
+df_stock_data = pd.DataFrame(stock_data)
+
+# Display the DataFrame
+print(df_stock_data)
 
 # print(symbols)      # ['META', 'AMZN', 'AAPL', 'TSLA']
 # print(alarm_limit)  # 2.50
