84a2b9fc78de5acb5e76ac49894c064f546facc6
Steve <sthlm_32@hotmail.com>
2025-03-21 15:11:22 +0100

    For loop

--- stalarm.py
+++ stalarm.py
@@ -4,29 +4,42 @@ import configparser
 
 os.system('cls')
 
-# Load the config file
-config = configparser.ConfigParser()
-config.read("config.ini")
+def read_config_ini():
+    # Load the config file
+    config = configparser.ConfigParser()
+    config.read("config.ini")
 
-# Read stock symbols
-symbols = config["stocks"]["symbols"].split(", ")  # Converts to a list
+    # Read stock symbols
+    symbols = config["stocks"]["symbols"].split(", ")  # Converts to a list
 
-# Read alarm limit as float
-alarm_limit = float(config["settings"]["alarm_limit"])
+    # Read alarm limit as float
+    alarm_limit = float(config["settings"]["alarm_limit"])
 
-print(symbols)      # ['META', 'AMZN', 'AAPL', 'TSLA']
-print(alarm_limit)  # 2.50
+    return symbols, alarm_limit
 
-# Here We are getting Facebook financial information
-# We need to pass FB as argument for that
-GetFacebookInformation = yahooFinance.Ticker("META")
+
+symbols, alarm_limit = read_config_ini()
+
+for symbol in symbols:
+    stock_info = yahooFinance.Ticker(symbol)
+    print(symbol)
+    latest_data = stock_info.history(period="1d")
+    print(latest_data["Open"].iloc[-1]) # -1 means "the last row" in the DataFrame
+    print(stock_info.fast_info["last_price"])  # Latest stock price
+
+# print(symbols)      # ['META', 'AMZN', 'AAPL', 'TSLA']
+# print(alarm_limit)  # 2.50
+
+# # Here We are getting Facebook financial information
+# # We need to pass FB as argument for that
+# GetFacebookInformation = yahooFinance.Ticker("META")
  
-# whole python dictionary is printed here
-# print(GetFacebookInformation.info)
-#print(GetFacebookInformation.history(period="1d"))
+# # whole python dictionary is printed here
+# # print(GetFacebookInformation.info)
+# #print(GetFacebookInformation.history(period="1d"))
 
-latest_data = GetFacebookInformation.history(period="1d")
+# latest_data = GetFacebookInformation.history(period="1d")
 
-print(latest_data["Open"].iloc[-1]) # -1 means "the last row" in the DataFrame
+# print(latest_data["Open"].iloc[-1]) # -1 means "the last row" in the DataFrame
 
-print(GetFacebookInformation.fast_info["last_price"])  # Latest stock price
+# print(GetFacebookInformation.fast_info["last_price"])  # Latest stock price
