7ece07114240dfb12fcd8de3840dab170b974185
Steve <sthlm_32@hotmail.com>
2025-03-24 17:40:58 +0100

    Working

--- stalarm.py
+++ stalarm.py
@@ -11,7 +11,6 @@ import pandas as pd
 import pytz
 
 # TODO add update_frequency to config.ini
-# TODO En sak jag tänker är att den ska ge larm vid ex 5% uppgång efter minst 10% nedgång.
 
 # Clear console
 os.system('cls')
@@ -48,6 +47,28 @@ def read_config_ini():
     return symbols, alarm_limit_decrease, alarm_limit_raise_after_decrease, start_date
 
 
+def convert_to_swedish_timezone(time):
+    """
+    Converts a given timestamp to Swedish time zone (CET/CEST).
+    
+    Args:
+        time (datetime): The datetime object to be converted.
+        
+    Returns:
+        datetime: The converted datetime object in Swedish time zone.
+    """
+    # Define Swedish time zone
+    swedish_timezone = pytz.timezone('Europe/Stockholm')
+    
+    # If the index (date) is not timezone-aware, localize it to UTC
+    if time.tzinfo is None:
+        time = pytz.utc.localize(time)
+    
+    # Convert the timestamp to Swedish time zone (CET/CEST)
+    return time.astimezone(swedish_timezone)
+
+
+
 # Read configuration
 symbols, alarm_limit_decrease, alarm_limit_raise_after_decrease, start_date = read_config_ini()
 
@@ -73,13 +94,13 @@ for symbol in symbols:
         
         if start_date in historical_data.index:
             # Define Swedish time zone
-            swedish_timezone = pytz.timezone('Europe/Stockholm')
+            # swedish_timezone = pytz.timezone('Europe/Stockholm')
 
             # Get the opening value for the specific date (start_date)
-            opening_value_historic = historical_data.loc[start_date, "Open"]
+            opening_value_historic = float(historical_data.loc[start_date, "Open"])
 
             # Get the lowest price in historical data
-            lowest_price_historical = historical_data["Low"].min()
+            lowest_price_historical = float(historical_data["Low"].min())
 
             # Get the date and time of the lowest price
             lowest_price_time = historical_data["Low"].idxmin()
@@ -89,20 +110,24 @@ for symbol in symbols:
                 lowest_price_time = pytz.utc.localize(lowest_price_time)
 
             # Convert the timestamp to Swedish time zone (CET/CEST)
-            lowest_price_time_swedish = lowest_price_time.astimezone(swedish_timezone)
+            #lowest_price_time_swedish = convert_to_swedish_timezone(lowest_price_time)
 
             # Get the highest price in historical data
-            highest_price_historical = historical_data["High"].min()
+            highest_price_historical = float(historical_data["High"].max())
 
             # Get the date and time of the highest price
-            highest_price_time = historical_data["High"].idxmin()
+            highest_price_time = historical_data["High"].idxmax()
 
             # If the index (date) is not timezone-aware, localize it to UTC
-            if lowest_price_time.tzinfo is None:
-                lowest_price_time = pytz.utc.localize(lowest_price_time)
+            if highest_price_time.tzinfo is None:
+                highest_price_time = pytz.utc.localize(highest_price_time)
 
             # Convert the timestamp to Swedish time zone (CET/CEST)
-            highest_price_time_swedish = lowest_price_time.astimezone(swedish_timezone)
+            #highest_price_time_swedish = convert_to_swedish_timezone(highest_price_time)
+
+            if(lowest_price_historical < opening_value_historic and ( (opening_value_historic - lowest_price_historical) * 100 > alarm_limit_decrease) ):
+                print(f"Lowest price {lowest_price_historical} is lower than opening value {opening_value_historic}")
+
         else:
             raise ValueError(f"No data available for {start_date}")
         
@@ -110,14 +135,16 @@ for symbol in symbols:
         msg_to_user = f"Error fetching data for {symbol}, Is this symbol correct? Check at https://finance.yahoo.com/lookup/"
         print(f"Error fetching data for {symbol}: {e}") #/////////////////////////////////////////////////////////////
         company_name = "N/A"
-        opening_value = "N/A"
-        last_value = "N/A"
-        percentage_value_change = "N/A"
         warning = False
 
     stock_data.append({
         "Symbol": symbol,
         "Company name": company_name,
+        "Opening value": opening_value_historic,
+        "Lowest price": lowest_price_historical,
+        "Lowest price time": lowest_price_time_swedish,
+        "Highest price": highest_price_historical,
+        "Highest price time": highest_price_time_swedish
     })
 
 # Create a DataFrame with the collected data
