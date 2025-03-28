043b7151fb1e70d22d1a7cccc71793cf181c3625
Steve <sthlm_32@hotmail.com>
2025-03-28 18:57:32 +0100

    No luck with Flask

--- app/hello.py
+++ app/hello.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    name = 'Rosalia'
-    return render_template('index.html', title='Welcome', username=name)
+    users = [ 'Rosalia','Adrianna','Victoria' ]
+    return render_template('index.html', title='Welcome', members=users)
 
 # @app.route("/get_data")
 # def getdata():
