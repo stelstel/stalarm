f86651a88f469a229941915df61b8411a7233ceb
Steve <sthlm_32@hotmail.com>
2025-03-28 17:47:49 +0100

    Starting with Flask

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    name = 'Rosalia'
+    return render_template('index.html', title='Welcome', username=name)
+
+# @app.route("/get_data")
+# def getdata():
+#     data = {
+#         'name' : 'My Name',
+#         'url' : 'My URL'
+#     }
+#     return json.dumps(data)
+
+if __name__ == "__main__":
+    app.run()
