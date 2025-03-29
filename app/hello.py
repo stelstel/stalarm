b8630460d1e373a2c4f7b87060debd16cb192328
Steve <sthlm_32@hotmail.com>
2025-03-29 11:52:59 +0100

    Webpage works

--- app/hello.py
+++ app/hello.py
@@ -1,22 +1,19 @@
 from flask import Flask
 from flask import render_template
 import json
+import stalarm
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    users = [ 'Rosalia','Adrianna','Victoria' ]
-    return render_template('index.html', title='Welcome', members=users)
+    users = [ 'Rosalia','Adrianna','Victoria' ] # ///////////////////
+    
 
-# @app.route("/get_data")
-# def getdata():
-#     data = {
-#         'name' : 'My Name',
-#         'url' : 'My URL'
-#     }
-#     return json.dumps(data)
+    # Convert DataFrame to HTML table
+    table_html = stalarm.df_stock_data.to_html(classes='table table-striped')
 
+    return render_template('index.html', table=table_html)
 if __name__ == "__main__":
     app.run()
